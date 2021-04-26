481fe69873c4a50f9ace95c14cac1522ff3b8227
wiryamanto <wiryamantoragil@gmail.com>
2021-04-26 17:02:40 +0700

    solve

--- src/controllers/foodsControllers.js
+++ src/controllers/foodsControllers.js
@@ -1,61 +1,61 @@
 // const {dbfoods} = require("../models/");
-const {foods} = require("../models/");
+const { foods } = require("../models/");
 
-module.exports={
-    getAllFoods: (req,res)=>{
-       
+module.exports = {
+    getAllFoods: (req, res) => {
+
         foods.findAll()
-        .then((data)=>{
-            res.send({
-                msg: "success",
-                status:"200",
-                data
+            .then((data) => {
+                res.send({
+                    msg: "success",
+                    status: "200",
+                    data
+                })
             })
-        .catch((err)=>{
-            res.send({
-                msg: "error",
-                status: "500",
-                err,
+            .catch((err) => {
+                res.send({
+                    msg: "error",
+                    status: "500",
+                    err,
+                });
             });
-        });    
-        });
     },
-    postFoods : (req,res)=>{
-        let {body}= req;
+    postFoods: (req, res) => {
+        let { body } = req;
         foods.create(body)
-        .then((data)=>{
-            res.status(200).send({
-                msg:"success post data",
-                status:"200",
-                data
+            .then((data) => {
+                res.status(200).send({
+                    msg: "success post data",
+                    status: "200",
+                    data
+                })
             })
-        })
-        .catch((err)=>{
-            res.status(500).send({
-                msg:"failed while post data",
-                status:500,
-                err,
-            });
-        });
-    },
-    getDataById :(req,res)=>{
-        let{id}= req.params;
-        foods.findOne({
-            where: {id}
-        })
-        .then((data)=>{
-            res.status(200).send({
-                msg:"Success Get Data By Id",
-                status:200,
-                data
-            })
-            .catch((err)=>{
+            .catch((err) => {
                 res.status(500).send({
-                    msg: "Failed While Get Data By Id",
+                    msg: "failed while post data",
                     status: 500,
                     err,
                 });
             });
+    },
+    getDataById: (req, res) => {
+        let { id } = req.params;
+        foods.findOne({
+            where: { id }
         })
+            .then((data) => {
+                res.status(200).send({
+                    msg: "Success Get Data By Id",
+                    status: 200,
+                    data
+                })
+                    .catch((err) => {
+                        res.status(500).send({
+                            msg: "Failed While Get Data By Id",
+                            status: 500,
+                            err,
+                        });
+                    });
+            })
     },
 };
