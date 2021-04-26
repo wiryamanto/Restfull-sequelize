530f4e970bc93e090255a455b9e16832d13aa00c
wiryamanto <wiryamantoragil@gmail.com>
2021-04-26 16:27:20 +0700

    fixed h10

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config({})
 const express = require ("express")
 const app = express();
-const port = process.env.Port || 3002
+const port = process.env.PORT || 3002
 const mainRoutes= require("./src/routes")
 
 app.use(express.urlencoded({extended: false}));
